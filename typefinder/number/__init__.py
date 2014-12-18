125a21721e244a78f28a8fc7731a7e1e7bef729f
Glen Harmon <glencharmon@gmail.com>
2014-12-18 21:49:22 +0100

    bug fixes

--- typefinder/number/__init__.py
+++ typefinder/number/__init__.py
@@ -1,1 +1,2 @@
 import integer
+import floating
--- typefinder/number/integer.py
+++ typefinder/number/integer.py
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
 
+from .. import generic
+
+def search(text):
+    integers = list()
+    text = ' '.join(unicode(text).split()).strip(',')
+    for token in text.split():
+        if valid(token):
+            integers.append(clean(token))
+    return integers
+
 def valid(number):
     try:
         number = float(number)
@@ -9,5 +19,5 @@ def valid(number):
 
 def clean(number):
     if valid(number):
-        return int(number)
+        return int(float(number))
     raise ValueError('{0} is not an integer'.format(number))
