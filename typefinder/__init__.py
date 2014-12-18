b257e9101f3291fa6c81cb97959273448c25b31b
Glen Harmon <glencharmon@gmail.com>
2014-12-18 21:47:02 +0100

    Added number types

--- typefinder/__init__.py
+++ typefinder/__init__.py
@@ -4,3 +4,6 @@ import ipv6
 import date
 import vlan
 import hostname
+import number
+
+all_types = (ipv4.ip, ipv4.network, ipv4.subnet, ipv4.wildcard, ipv4.ip_range, hostname, vlan, date, number.integer, number.floating)
